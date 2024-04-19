07a06f3490bd1a5c547df39d306ef770f5700d21
caowent <caowent@163.com>
2024-04-19 16:04:52 +0800

    只保留驱动，移除demo

--- msp_i2c.c
+++ msp_i2c.c
@@ -1,32 +1,31 @@
 /**
-  ******************************************************************************
-  * @file    msp_i2c
-  * @author  曹文涛
-  * @date    2020-2-20
-  * @brief   提供GPIO模拟I2C驱动
-  *****************************************************************************/
+ ******************************************************************************
+ * @file    msp_i2c
+ * @author  曹文涛
+ * @date    2020-2-20
+ * @brief   提供GPIO模拟I2C驱动
+ *****************************************************************************/
 
 #include "msp_i2c.h"
 
 /* 写控制位 */
-#define I2C_W  (0)
+#define I2C_W (0)
 /* 读控制位 */
-#define I2C_R  (1)
+#define I2C_R (1)
 
 /**** 引脚控制宏 ****/
-#define I2C_SCL_H()     HAL_GPIO_WritePin(I2C_SCL_GPIO_Port, I2C_SCL_Pin, GPIO_PIN_SET)
-#define I2C_SCL_L()     HAL_GPIO_WritePin(I2C_SCL_GPIO_Port, I2C_SCL_Pin, GPIO_PIN_RESET)
+#define I2C_SCL_H() HAL_GPIO_WritePin(I2C_SCL_GPIO_Port, I2C_SCL_Pin, GPIO_PIN_SET)
+#define I2C_SCL_L() HAL_GPIO_WritePin(I2C_SCL_GPIO_Port, I2C_SCL_Pin, GPIO_PIN_RESET)
 
-#define I2C_SDA_H()     HAL_GPIO_WritePin(I2C_SDA_GPIO_Port, I2C_SDA_Pin, GPIO_PIN_SET)
-#define I2C_SDA_L()     HAL_GPIO_WritePin(I2C_SDA_GPIO_Port, I2C_SDA_Pin, GPIO_PIN_RESET)
+#define I2C_SDA_H() HAL_GPIO_WritePin(I2C_SDA_GPIO_Port, I2C_SDA_Pin, GPIO_PIN_SET)
+#define I2C_SDA_L() HAL_GPIO_WritePin(I2C_SDA_GPIO_Port, I2C_SDA_Pin, GPIO_PIN_RESET)
 
-#define I2C_SDA_READ()  HAL_GPIO_ReadPin(I2C_SDA_GPIO_Port, I2C_SDA_Pin)
-
+#define I2C_SDA_READ() HAL_GPIO_ReadPin(I2C_SDA_GPIO_Port, I2C_SDA_Pin)
 
 static void i2c_delay(void)
 {
     /* 主频过高需加 __NOP()演示，数量依需求而定 */
-	/* 高速I2C 一般支持400kHz，多数情况不需要加延时 */
+    /* 高速I2C 一般支持400kHz，多数情况不需要加延时 */
 }
 
 /* 主节点发起的I2C启动信号 */
@@ -37,7 +36,7 @@ static void i2c_start(void)
 
     I2C_SCL_H();
     i2c_delay();
-    
+
     I2C_SDA_L();
     i2c_delay();
 }
@@ -50,7 +49,7 @@ static void i2c_stop(void)
 
     I2C_SCL_H();
     i2c_delay();
-    
+
     I2C_SDA_H();
     i2c_delay();
 }
@@ -58,11 +57,11 @@ static void i2c_stop(void)
 /* 发送一个字节 */
 static void send_byte(uint8_t byte)
 {
-    for(uint32_t i=0; i<8; i++)
+    for (uint32_t i = 0; i < 8; i++)
     {
         I2C_SCL_L();
-        
-        if(byte&0x80)
+
+        if (byte & 0x80)
         {
             I2C_SDA_H();
         }
@@ -74,7 +73,7 @@ static void send_byte(uint8_t byte)
 
         I2C_SCL_H();
         i2c_delay();
-        
+
         byte <<= 1;
     }
 }
@@ -83,27 +82,27 @@ static void send_byte(uint8_t byte)
 static uint8_t read_byte(void)
 {
     uint8_t byte = 0;
-    
+
     I2C_SCL_L();
     I2C_SDA_H();
     i2c_delay();
 
-    for(uint32_t i=0; i<8; i++)
+    for (uint32_t i = 0; i < 8; i++)
     {
         byte <<= 1;
-        
+
         I2C_SCL_L();
         i2c_delay();
-        
+
         I2C_SCL_H();
         i2c_delay();
-        
-        if(I2C_SDA_READ())
+
+        if (I2C_SDA_READ())
         {
             byte |= 0x01;
         }
     }
-    
+
     return byte;
 }
 
@@ -111,19 +110,19 @@ static uint8_t read_byte(void)
 static uint8_t wait_ack(void)
 {
     uint8_t ack = 0;
-    
+
     I2C_SCL_L();
     I2C_SDA_H();
     i2c_delay();
-    
+
     I2C_SCL_H();
     i2c_delay();
-    
+
     ack = I2C_SDA_READ();
-    
+
     I2C_SCL_L();
     i2c_delay();
-    
+
     return ack;
 }
 
@@ -131,13 +130,13 @@ static uint8_t wait_ack(void)
 static void send_ack(void)
 {
     I2C_SCL_L();
-    
+
     I2C_SDA_L();
     i2c_delay();
 
     I2C_SCL_H();
     i2c_delay();
-    
+
     I2C_SCL_L();
     I2C_SDA_H();
     i2c_delay();
@@ -147,13 +146,13 @@ static void send_ack(void)
 static void send_nack(void)
 {
     I2C_SCL_L();
-    
+
     I2C_SDA_H();
     i2c_delay();
 
     I2C_SCL_H();
     i2c_delay();
-    
+
     I2C_SCL_L();
     I2C_SDA_H();
     i2c_delay();
@@ -163,7 +162,7 @@ static void send_nack(void)
 void msp_i2c_init(void)
 {
     /* 此处无需初始化GPIO,cube已自动生成 */
-    
+
     /* 释放总线 */
     i2c_stop();
 }
@@ -173,43 +172,43 @@ void msp_i2c_init(void)
 uint8_t msp_i2c_check(uint8_t dev)
 {
     uint8_t ack = 0;
-    
+
     /* I2C启动 */
     i2c_start();
-    
+
     /* 发送地址 */
-    send_byte(dev|I2C_W);
-    
+    send_byte(dev | I2C_W);
+
     /* 获取ack */
     ack = wait_ack();
-    
+
     /* I2C停止 */
     i2c_stop();
-    
+
     return ack;
 }
 
 /* 主节点写数据 */
-uint8_t msp_i2c_write_bytes(uint8_t dev, uint16_t addr, uint8_t addr_is_8b, 
-uint8_t* buf, uint16_t len)
+uint8_t msp_i2c_write_bytes(uint8_t dev, uint16_t addr, uint8_t addr_is_8b,
+                            uint8_t *buf, uint16_t len)
 {
     uint8_t err = 0;
-    
+
     /* I2C启动 */
     i2c_start();
-    
+
     /* 发送设备地址 */
-    send_byte(dev|I2C_W);
-    
+    send_byte(dev | I2C_W);
+
     /* 等待ack */
-    if(wait_ack())
+    if (wait_ack())
     {
         err = 1;
         goto LABEL_END;
     }
-    
+
     /* 如果寄存器是8位地址 */
-    if(addr_is_8b)
+    if (addr_is_8b)
     {
         /* 发送寄存器地址 */
         send_byte(addr);
@@ -218,68 +217,68 @@ uint8_t* buf, uint16_t len)
     /* 如果寄存器是16位地址 */
     {
         /* 先发高8位地址 */
-        send_byte(addr>>8);
-        
+        send_byte(addr >> 8);
+
         /* 等待ack */
-        if(wait_ack())
+        if (wait_ack())
         {
             err = 1;
             goto LABEL_END;
         }
-        
+
         /* 再发低8位地址 */
         send_byte(addr);
     }
-    
+
     /* 等待ack */
-    if(wait_ack())
+    if (wait_ack())
     {
         err = 1;
         goto LABEL_END;
     }
-    
+
     /* 发送数据 */
-    for(int i=0; i<len; i++)
+    for (int i = 0; i < len; i++)
     {
         /* 发送单个字节 */
-        send_byte(*(buf+i));
-        
+        send_byte(*(buf + i));
+
         /* 等待ack */
-        if(wait_ack())
+        if (wait_ack())
         {
             err = 1;
             goto LABEL_END;
         }
     }
-    
+
 LABEL_END:
     /* I2C停止 */
     i2c_stop();
-    
+
     return err;
 }
 
 /* 主节点读数据 */
-uint8_t msp_i2c_read_bytes(uint8_t dev, uint16_t addr, uint8_t addr_is_8b, 
-uint8_t* buf, uint16_t len)
+uint8_t msp_i2c_read_bytes(uint8_t dev, uint16_t addr, uint8_t addr_is_8b,
+                           uint8_t *buf, uint16_t len)
 {
     uint8_t err = 0;
-    
+
     /* I2C启动 */
     i2c_start();
-    
+
     /* 发送设备地址 */
-    send_byte(dev|I2C_W);
-    
+    send_byte(dev | I2C_W);
+
     /* 等待ack */
-    if(wait_ack())
+    if (wait_ack())
     {
         err = 1;
         goto LABEL_END;
     }
-    
+
     /* 如果寄存器是8位地址 */
-    if(addr_is_8b)
+    if (addr_is_8b)
     {
         /* 发送寄存器地址 */
         send_byte(addr);
@@ -288,60 +287,75 @@ uint8_t* buf, uint16_t len)
     /* 如果寄存器是16位地址 */
     {
         /* 先发高8位地址 */
-        send_byte(addr>>8);
-        
+        send_byte(addr >> 8);
+
         /* 等待ack */
-        if(wait_ack())
+        if (wait_ack())
         {
             err = 1;
             goto LABEL_END;
         }
-    
+
         /* 再发低8位地址 */
         send_byte(addr);
     }
-    
+
     /* 等待ack */
-    if(wait_ack())
+    if (wait_ack())
     {
         err = 1;
         goto LABEL_END;
     }
-    
+
     /* I2C启动 */
     i2c_start();
-    
+
     /* 切换成主节点读取模式 */
-    send_byte(dev|I2C_R);
-    
+    send_byte(dev | I2C_R);
+
     /* 等待ack */
-    if(wait_ack())
+    if (wait_ack())
     {
         err = 1;
         goto LABEL_END;
     }
-    
+
     /* 读取数据 */
-    for(int i=0; i<(len-1); i++)
+    for (int i = 0; i < (len - 1); i++)
     {
         /* 读取单个字节 */
-        *(buf+i) = read_byte();
-        
+        *(buf + i) = read_byte();
+
         /* 发送ack */
         send_ack();
     }
-    
+
     /* 读取最后一个字节 */
-    *(buf+len-1) = read_byte();
+    *(buf + len - 1) = read_byte();
     /* 发送nack，结束读取 */
     send_nack();
-    
+
 LABEL_END:
     /* I2C停止 */
     i2c_stop();
-    
+
     return err;
 }
 
+/* 主节点写单字节数据 */
+uint8_t msp_i2c_write_byte(uint8_t dev, uint16_t addr, uint8_t byte)
+{
+    return msp_i2c_write_bytes(dev, addr, ADDR_IS_8b, &byte, 1);
+}
+
+/* 主节点读单字节数据 */
+uint8_t msp_i2c_read_byte(uint8_t dev, uint16_t addr)
+{
+    uint8_t byte;
+
+    msp_i2c_read_bytes(dev, addr, ADDR_IS_8b, &byte, 1);
+
+    return byte;
+}
 
 /*********************************** END OF FILE ***********************************/
---
+++ msp_i2c.h
@@ -0,0 +1,40 @@
+/**
+ ******************************************************************************
+ * @file    msp_i2c
+ * @author  曹文涛
+ * @date    2020-2-20
+ * @brief   提供GPIO模拟I2C驱动
+ *****************************************************************************/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MSP_I2C_PROBE_H
+#define __MSP_I2C_PROBE_H
+
+#include "common.h"
+
+#define ADDR_IS_8b (1)
+#define ADDR_IS_16b (0)
+
+/* i2c初始化 */
+void msp_i2c_init(void);
+
+/* 校验从节点 */
+uint8_t msp_i2c_check(uint8_t dev);
+
+/* 主节点写数据 */
+uint8_t msp_i2c_write_bytes(uint8_t dev, uint16_t addr, uint8_t addr_is_8b,
+                            uint8_t *buf, uint16_t len);
+
+/* 主节点读数据 */
+uint8_t msp_i2c_read_bytes(uint8_t dev, uint16_t addr, uint8_t addr_is_8b,
+                           uint8_t *buf, uint16_t len);
+
+/* 主节点写单字节数据 */
+uint8_t msp_i2c_write_byte(uint8_t dev, uint16_t addr, uint8_t byte);
+
+/* 主节点读单字节数据 */
+uint8_t msp_i2c_read_byte(uint8_t dev, uint16_t addr);
+
+#endif /* __MSP_I2C_PROBE_H */
+
+/*********************************** END OF FILE ***********************************/
